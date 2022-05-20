62d31973ef8998c1ceeb9eb33f125e9c0d365b91
Danilo Dell'Orco <danilodellorcolp@gmail.com>
2022-05-20 09:59:36 +0200

    query3 90%

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -1,11 +1,14 @@
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.Optional;
+import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.api.java.function.Function2;
 import scala.Tuple2;
 import utils.TaxiRow;
-
+import utils.ValQ3;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,20 +19,20 @@ public class Testing {
     private static int count_null = 0;
     private static List query1_results = new ArrayList();
     //    private static String sparkURL = "spark://spark-master:7077";
-//    private static String hdfsURL = "hdfs://hdfs-master:54310";
+    //    private static String hdfsURL = "hdfs://hdfs-master:54310";
     private static String sparkURL = "local";
     private static String hdfsURL = "data";
     private static String dataset1_path = hdfsURL + "/yellow_tripdata_2021-12.parquet";
     private static String dataset2_path = hdfsURL + "/yellow_tripdata_2022-01.parquet";
     private static String dataset3_path = hdfsURL + "/yellow_tripdata_2022-02.parquet";
 
+    //TODO vedere il caching
     public static void main(String[] args) {
         SparkSession spark = SparkSession
                 .builder()
                 .master(sparkURL)
                 .appName("Java Spark SQL basic example")
                 .getOrCreate();
-        JavaSparkContext sc = new JavaSparkContext();
 
         // QUERY 1
         /*
@@ -43,7 +46,7 @@ public class Testing {
         }
         System.out.println("=============================================================");
          */
-        query3(spark, dataset1_path);
+        query3_pro(spark, dataset1_path);
         promptEnterKey();
     }
 
@@ -55,7 +58,7 @@ public class Testing {
         JavaRDD<Row> rowRDD = spark.read().option("header", "false").parquet(dataset).toJavaRDD();
         JavaRDD<TaxiRow> taxiRows = rowRDD.map(r -> ParseRow(r));
 
-        // Analisys of Month 1
+        // Analisys of the single Month
         JavaRDD<Double> tips = taxiRows.map(t -> t.getTip_amount());
         JavaRDD<Double> total = taxiRows.map(t -> t.getTotal_amount());
         JavaRDD<Double> tolls = taxiRows.map(t -> t.getTolls_amount());
@@ -115,5 +118,81 @@ public class Testing {
             System.out.println(String.format("%d) %d | %d",i,t._2(),t._1()));
             i++;
         }
+    }
+
+    public static void query3_pro(SparkSession spark, String dataset) {
+        JavaRDD<Row> rowRDD = spark.read().option("header", "false").parquet(dataset).toJavaRDD();
+        JavaRDD<TaxiRow> taxis = rowRDD.map(r -> ParseRow(r));
+
+        JavaPairRDD<Long, ValQ3> occurrences = taxis.mapToPair(
+                r -> new Tuple2<>(r.getDOLocationID(),
+                new ValQ3(r.getPassenger_count(),r.getFare_amount(),1)));
+
+        JavaPairRDD<Long, ValQ3> reduced = occurrences.reduceByKey((Function2<ValQ3, ValQ3, ValQ3>) (v1, v2) -> {
+            Double pass = v1.getPassengers()+v2.getPassengers();
+            Double fare = v1.getFare()+v2.getFare();
+            Integer occ = v1.getOccurrences()+v2.getOccurrences();
+            ValQ3 v = new ValQ3(pass,fare,occ);
+            return v;
+        });
+
+
+        // ValQ3 = {pass,fare,occurr}
+        JavaPairRDD<Long, ValQ3> statistics = occurrences.mapToPair(
+                r -> {
+                    Integer num_occurrences = r._2().getOccurrences();
+                    Double pass_mean = r._2().getPassengers()/num_occurrences;
+                    Double fare_mean = r._2().getFare()/num_occurrences;
+
+                    return new Tuple2<>(r._1(),
+                    new ValQ3(pass_mean,fare_mean,num_occurrences));
+                });
+
+        JavaPairRDD<Long, Tuple2<ValQ3, Optional<ValQ3>>> joined = occurrences.leftOuterJoin(statistics);
+
+        System.out.println("JOINEEEEEEED");
+        joined.foreach((VoidFunction<Tuple2<Long, Tuple2<ValQ3, Optional<ValQ3>>>>) r->System.out.println(r.toString()));
+
+
+        JavaPairRDD<Long, ValQ3> iterations = joined.mapToPair(
+                r -> {
+                    Double fare_mean = r._2()._2().get().getFare();
+                    Double fare_val  = r._2()._1().getFare();
+                    Double fare_dev  = Math.pow((fare_val-fare_mean),2);
+                    r._2()._2().get().setFare_stddev(fare_dev);
+
+                    return new Tuple2<>(r._1(), r._2()._2().get());
+                });
+
+        JavaPairRDD<Long, ValQ3> deviation = iterations.reduceByKey((Function2<ValQ3, ValQ3, ValQ3>) (v1, v2) -> {
+            Double fare_total_stddev = v1.getFare_stddev()+v2.getFare_stddev();
+            ValQ3 v = new ValQ3(fare_total_stddev);
+            return v;
+        });
+
+
+        /*
+        JavaPairRDD<Integer,Long> swapped = reduced.mapToPair(x -> new Tuple2<>(x._2(), x._1()));
+        System.out.println("\n\nSWAPPED");
+        swapped.foreach(data -> {
+            System.out.println("occurrences=" + data._1() + " DOLocationID=" + data._2());
+        });
+
+
+
+        JavaPairRDD<Integer,Long> sorted = swapped.sortByKey(false);
+        System.out.println("\n\nSORTED");
+        sorted.foreach(data -> {
+            System.out.println("occurrences=" + data._1() + " DOLocationID=" + data._2());
+        });
+
+        List<Tuple2<Integer,Long>> top = sorted.take(5);
+        System.out.println("\n\nTOP");
+        int i =0;
+        for (Tuple2<Integer,Long> t:top){
+            System.out.println(String.format("%d) %d | %d",i,t._2(),t._1()));
+            i++;
+        }
+         */
     }
 }

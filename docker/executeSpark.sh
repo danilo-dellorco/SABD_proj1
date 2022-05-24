541b1941ad3be9899a4ab1a36d21c3fe51040d4a
Danilo Dell'Orco <danilodellorcolp@gmail.com>
2022-05-24 15:20:04 +0200

    mongodriver

--- docker/executeSpark.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-docker-compose exec spark-master /bin/sh -c "./bin/spark-submit --class $1 --master spark://spark-master:7077 hdfs://hdfs-master:54310/sabd-proj-1.0.jar"
--- docker/start-mongo-client.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -i -t --name mongo_cli --network=mongonet mongo:latest /bin/bash

973f54a584e91c7f5390f7aeffa580f3a10285ab
Danilo Dell'Orco <danilodellorcolp@gmail.com>
2022-05-25 13:02:10 +0200

    removed scripts

--- src/main/java/queries/Query2.java
+++ src/main/java/queries/Query2.java
@@ -12,7 +12,6 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.bson.Document;

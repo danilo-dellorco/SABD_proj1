8b7a6d58554def286bb22b75f6fd0466c9c3cd03
Michele <40394496+michsalvv@users.noreply.github.com>
2022-06-13 21:36:02 +0200

    Update stop-nifi-flow.sh

--- docker/stop-nifi-flow.sh
+++ docker/stop-nifi-flow.sh
@@ -2,5 +2,3 @@
 
 # Stop Nifi Flow when receive the signal at the end of the injection in HDFS
 curl -i -X PUT -H 'Content-Type: application/json' -d '{"id":"e7c1e05b-0180-1000-39fe-f94ef5456a54","state":"STOPPED"}' http://localhost:8090/nifi-api/flow/process-groups/e7c1e05b-0180-1000-39fe-f94ef5456a54;
-sudo docker stop nifi-master;
-sudo docker rm nifi-master;
